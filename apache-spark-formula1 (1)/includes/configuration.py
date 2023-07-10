58a318bff1f976e7c511ee0d9fb3f7e3588e6645
guido121 <guido121>
2023-07-10 20:42:32 +0000

    update

---
+++ apache-spark-formula1 (1)/includes/configuration.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+raw_folder_path = '/FileStore/dataset/apache-spark-formula1/raw'
+processed_folder_path = '/FileStore/dataset/apache-spark-formula1/processed'
+presentation_folder_path = '/FileStore/dataset/apache-spark-formula1/presentation'
+
+# COMMAND ----------
+
+# raw_folder_path = 'abfss://raw@sa7090375.dfs.core.windows.net'
+# processed_folder_path = 'abfss://processed@sa7090375.dfs.core.windows.net'
+# presentation_folder_path = 'abfss://presentation@sa7090375.dfs.core.windows.net'
